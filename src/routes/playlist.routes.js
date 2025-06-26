63f24f939b4dbea93460ad9499cc8acb9ee22a06
pyschonoice <monukk2002@gmail.com>
2025-06-26 14:52:32 +0530

    smol change

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -17,7 +17,7 @@ router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 router.route("/").post(createPlaylist);
 
 router
-  .route("/:playlistId")
+  .route("/:playlistId") // get one specific playlist
   .get(getPlaylistById)
   .patch(updatePlaylist)
   .delete(deletePlaylist);
@@ -25,6 +25,6 @@ router
 router.route("/add/:videoId/:playlistId").patch(addVideoToPlaylist);
 router.route("/remove/:videoId/:playlistId").patch(removeVideoFromPlaylist);
 
-router.route("/user/:userId").get(getUserPlaylists);
+router.route("/user/:userId").get(getUserPlaylists); // get all user playlists
 
 export default router;
--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,15 +1,15 @@
-import { Router } from "express";
-import * as subscriptionController from "../controllers/subscriber.controller.js";
-import * as authMiddleware from "../middlewares/auth.middleware.js"
+  import { Router } from "express";
+  import * as subscriptionController from "../controllers/subscriber.controller.js";
+  import * as authMiddleware from "../middlewares/auth.middleware.js"
 
-const router = Router()
+  const router = Router()
 
-router.use(authMiddleware.verifyJWT)
+  router.use(authMiddleware.verifyJWT)
 
-router.route("/c/:username")
-  .get(subscriptionController.getUserChannelSubscribers)
-  .post(subscriptionController.toggleSubscription);
+  router.route("/c/:username")
+    .get(subscriptionController.getUserChannelSubscribers)
+    .post(subscriptionController.toggleSubscription);
 
-router.get("/u/:username",subscriptionController.getSubscribedChannels);
+  router.get("/u/:username",subscriptionController.getSubscribedChannels);
 
-export default router;
+  export default router;
